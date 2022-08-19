ebab4b7600aa73caf989cde8a93cb962a3fdd14c
Pavel Kirillov <68268029+Alfredovich274@users.noreply.github.com>
2022-08-19 19:49:48 +0300

    Проект django парсер hh с условиями. Добавлена заполнение базы данных вакансий

---
+++ website/parser_hh/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Company, City, Vacancy, Skill, Salary, Schedule,\
+    Experience, Employment
+
+
+admin.site.register(Company)
+admin.site.register(City)
+admin.site.register(Vacancy)
+admin.site.register(Skill)
+admin.site.register(Salary)
+admin.site.register(Schedule)
+admin.site.register(Experience)
+admin.site.register(Employment)
---
+++ website/parser_hh/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+
+
+# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=32, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=64, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=16, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    address = models.TextField(max_length=128, blank=True)
+    contact = models.TextField(max_length=128, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Employment(models.Model):
+    name = models.CharField(max_length=16, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Experience(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Salary(models.Model):
+    name = models.CharField(max_length=32, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Schedule(models.Model):
+    name = models.CharField(max_length=16, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField(blank=True)  # Описание
+    # Трудоустройство
+    employment = models.ForeignKey(Employment, on_delete=models.CASCADE,
+                                   blank=True)
+    # Опыт
+    experience = models.ForeignKey(Experience, on_delete=models.CASCADE,
+                                   blank=True)
+    # Зарплата
+    salary = models.ForeignKey(Salary, on_delete=models.CASCADE, blank=True)
+    # Тип занятости
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE, blank=True)
+    # Что нужно
+    requirement = models.TextField(blank=True)
+    # и чем придется заниматься
+    responsibility = models.TextField(blank=True)
+    # роли
+    role = models.CharField(max_length=64, blank=True)
+    # специализация
+    specialization = models.ManyToManyField(Specialization, blank=True)
+    url = models.CharField(max_length=64, blank=True)
+    skill = models.ManyToManyField(Skill, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
